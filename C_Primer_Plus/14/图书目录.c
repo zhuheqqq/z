fb98cdc186b16332050e9e7c7785cc76407af13f
zhu-to-u <2528487925@qq.com>
2022-12-01 20:22:10 +0800

    结构体

--- C_Primer_Plus/14/图书目录.c
+++ C_Primer_Plus/14/图书目录.c
@@ -1,2 +1,25 @@
 #include<stdio.h>
-struct 
+struct Times{
+    int year;
+    int month;
+    int day;
+}ti;
+struct Contents{
+    char name[40];
+    double price;
+    char writer[40];
+    struct Times ti;
+}con;
+int main(int argc,int argv[])
+{
+    struct Times ti;
+    struct Contents con;
+    gets(con.name);
+    scanf("%lf\n",&con.price);
+    gets(con.writer);
+    scanf("%d-%d-%d",&con.ti.year,&con.ti.month,&con.ti.day);
+    puts(con.name);
+    puts(con.writer);
+    printf("%f %d-%02d-%02d",con.price,con.ti.year,con.ti.month,con.ti.day);
+
+}
