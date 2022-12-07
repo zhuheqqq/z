08079b052e7645fd659dd225675c7e6aefa35191
zhu-to-u <2528487925@qq.com>
2022-12-08 05:46:11 +0800

    pta

--- pta/期末.c
+++ pta/期末.c
@@ -12,27 +12,44 @@
 //     printf("%d",j);
 //     return 0;
 // }
+// #include<stdio.h>
+// #include<stdlib.h>
+// #include<string.h>
+// int main(void)
+// {
+//     int n,m,i,j;
+//     scanf("%d %d",&n,&m);
+//     int a[n][m];
+//     int b[n];
+//     memset(b,0,sizeof(b));
+//     for(i=0;i<n;i++){
+//         for(j=0;j<m;j++){
+//             scanf("%d",&a[i][j]);
+//         }
+//     }
+//     for(i=0;i<n;i++){
+//         for(j=0;j<m;j++){
+//             b[i]+=a[i][j];
+//         }
+//     }
+//     for(i=0;i<n;i++){
+//         printf("%d\n",b[i]);
+//     }
+// }
 #include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include<math.h>
 int main(void)
 {
-    int n,m,i,j;
-    scanf("%d %d",&n,&m);
-    int a[n][m];
-    int b[n];
-    memset(b,0,sizeof(b));
-    for(i=0;i<n;i++){
-        for(j=0;j<m;j++){
-            scanf("%d",&a[i][j]);
-        }
+    double a,b,c;
+    double s,area;
+    scanf("%lf %lf %lf",&a,&b,&c);
+    if(a+b>c&&a+c>b&&b+c>a){
+        s=(a+b+c)/2;
+        area=sqrt(s*(s-a)*(s-b)*(s-c));
+        printf("area = %.2f; perimeter = %.2f",area,2*s);
+    }else{
+        printf("These sides do not correspond to a valid triangle");
     }
-    for(i=0;i<n;i++){
-        for(j=0;j<m;j++){
-            b[i]+=a[i][j];
-        }
-    }
-    for(i=0;i<n;i++){
-        printf("%d\n",b[i]);
-    }
+    return 0;
+
 }
