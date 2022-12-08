e26a773e3994277a888ecb2a122edc902416937f
zhu-to-u <2528487925@qq.com>
2022-12-08 16:40:13 +0800

    pta

--- pta/期末/期末.c
+++ pta/期末/期末.c
@@ -81,40 +81,79 @@
 //     }
 //     return 0;
 // }
-#include<stdio.h>
-#include<stdlib.h>
-int main(void)
-{
-    int n,i,j,k=0,m=0,max,t,cnt=0;
-    scanf("%d",&n);
-    int (*a)[6]=(int(*)[6])malloc(sizeof(int)*n*6);
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            scanf("%d",&a[i][j]);
+// #include<stdio.h>
+// #include<stdlib.h>
+// int main(void)
+// {
+//     int n,i,j,k=0,m=0,max,t,cnt=0;
+//     scanf("%d",&n);
+//     int (*a)[6]=(int(*)[6])malloc(sizeof(int)*n*6);
+//     for(i=0;i<n;i++){
+//         for(j=0;j<n;j++){
+//             scanf("%d",&a[i][j]);
+//         }
+//     }
+//     for(i=0;i<n;i++){
+//         cnt=0;
+//         max=a[i][0];
+//         for(j=0;j<n;j++){
+//             if(a[i][j]>max){
+//                 max=a[i][j];
+//                 k=i;
+//                 m=j;
+//             }
+//             for(t=0;t<n;t++){
+//                 if(max>a[t][j]){
+//                     cnt=1;
+//                 }
+//             }
+//             if(cnt==0){
+//                 break;
+//             }
+//         }
+//     }
+//     if(cnt==1){
+//         printf("NONE");
+//     }else{
+//         printf("%d %d",k,m);
+//     }
+// }
+#include <stdio.h>
+#define MAXN 6
+
+int main(void) {
+    int n, matrix[MAXN][MAXN], i, j, k, i_max, j_min;
+
+    scanf("%d", &n);  //输入阶数
+    for (i = 0; i < n; i++) {  //输入数据
+        for (j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
         }
     }
-    for(i=0;i<n;i++){
-        cnt=0;
-        max=a[i][0];
-        for(j=0;j<n;j++){
-            if(a[i][j]>max){
-                max=a[i][j];
-                k=i;
-                m=j;
+    for (i = 0; i < n; i++) {
+        i_max = matrix[i][0];
+        for (j = 0; j < n; j++) {  //找到该行最大数
+            if (matrix[i][j] > i_max) {
+                i_max = matrix[i][j];
             }
-            for(t=0;t<n;t++){
-                if(max>a[t][j]){
-                    cnt=1;
+        }
+        for (j = 0; j < n; j++) {
+            j_min = matrix[0][j];
+            for (k = 0; k < n; k++) {  //找到该列最小数
+                if (matrix[k][j] < j_min) {
+                    j_min = matrix[k][j];
                 }
             }
-            if(cnt==0){
-                break;
+            if (matrix[i][j] == i_max) {
+                if (matrix[i][j] == j_min) {
+                    printf("%d %d\n", i, j);
+
+                    return 0;
+                }
             }
         }
     }
-    if(cnt==1){
-        printf("NONE");
-    }else{
-        printf("%d %d",k,m);
-    }
+    printf("NONE\n");
+
+    return 0;
 }
