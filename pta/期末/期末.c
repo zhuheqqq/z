d9c2976ea59f5405ed00735839db3b830d8cbaf0
zhu-to-u <2528487925@qq.com>
2022-12-09 23:47:44 +0800

    pta

--- pta/期末/期末.c
+++ pta/期末/期末.c
@@ -200,22 +200,22 @@
 // }
 
 
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-int main(void)
-{
-    char ch[80];
-    int a[127]={0};
-    int i;
-    gets(ch);
-    for(i=0;i<strlen(ch);i++){
-        a[ch[i]]++;
-    }
-    for(i=0;i<127;i++){
-        if(a[i]!=0){
-            printf("%c",i);
-        }
-    }
-    return 0;
-}
+// #include<stdio.h>
+// #include<string.h>
+// #include<stdlib.h>
+// int main(void)
+// {
+//     char ch[80];
+//     int a[127]={0};
+//     int i;
+//     gets(ch);
+//     for(i=0;i<strlen(ch);i++){
+//         a[ch[i]]++;
+//     }
+//     for(i=0;i<127;i++){
+//         if(a[i]!=0){
+//             printf("%c",i);
+//         }
+//     }
+//     return 0;
+// }
