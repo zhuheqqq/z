35d1309f1eb9493e0deca04776279c949fa8e5ac
zhu-to-u <2528487925@qq.com>
2023-02-20 21:07:14 +0800

    username.c

---
+++ LINUX系统编程/username.c
@@ -0,0 +1,27 @@
+//ls -l获得name
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<unistd.h>
+#include<pwd.h>
+
+int main(int argc,char **argv)
+{
+	struct passwd *pwdline;
+
+	if(argc<2)
+	{
+		fprintf(stderr,"Usage...\n");
+		exit(1);
+	}
+
+	pwdline=getpwuid(atoi(argv[1]));
+
+	puts(pwdline->pw_name);
+
+
+
+
+	exit(0);
+}
