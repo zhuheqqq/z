79bea510758687e401e81cc48836fdbf081fe7c4
zhu-to-u <2528487925@qq.com>
2023-03-15 20:55:50 +0800

    myshell

--- myshell/smsh/process.c
+++ myshell/smsh/process.c
@@ -6,9 +6,13 @@ int process(char ** args)
     int rv=0;
     if(args[0]==NULL)
     rv=0;
-    else if(is_control_command(args[0]))
-    rv=do_control_command(args);
-    else if(ok_to_execute())
-    rv=execute(args);
+    else if(is_control_command(args[0])){
+        rv=do_control_command(args);
+    }
+    else if(ok_to_execute()){
+        if(!builtin_command(args,&rv)){
+            rv=execute(args);
+        }
+    }
     return rv;
 }
