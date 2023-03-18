0bafeff138b6ec4fb5fa7ba29e96dec44f779b61
zhu-to-u <2528487925@qq.com>
2023-03-18 15:56:14 +0800

    myshell

--- myshell/zhqshell.c
+++ myshell/zhqshell.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include<unistd.h>
+#include<unistd.h>//chdir()所在头文件
 #include<signal.h>
 #include<sys/wait.h>
 #include<ctype.h>
@@ -10,30 +10,20 @@
 #define YES 1
 #define NO 0
 
-#define DFL_PROMPT "zhq_shell $ "
+#define DFL_PROMPT "zhq_shell~$ "
 
-#define is_delim(x) ((x)==' '||(x)=='\t')
+#define MAX 128
 
-char *next_cmd();
-char **splitline(char*);
-void freelist(char**);
-void *emalloc(size_t);
-void *erealloc(void *,size_t);
-int execute(char **);
-void fatal(char *,char *,int);
+int pass=0;//标记&
 
 
-enum states {NEUTRAL,WANT_THEN,THEN_BLOCK};//枚举
-enum results {SUCCESS,FAIL};
 
-static int if_state=NEUTRAL;//0
-static int if_result=SUCCESS;
-static int last_stat=0;
 
 int main()
 {
-    char *cmdline,*prompt,**arglist;
-    int result;
+    char *cmdline,*prompt,**argv=NULL;
+    int i=1;
+    const char *mark=" ";
     void setup();//忽略信号
 
     prompt=DFL_PROMPT;
@@ -41,10 +31,12 @@ int main()
 
     while((cmdline=next_cmd(prompt,stdin))!=NULL)
     {
-        if((arglist=splitline(cmdline))!=NULL){
-            result=process(arglist);
-            freelist(arglist);
+        argv[0]=strtok(cmdline,mark);
+        while(argv[i]=strtok(NULL,mark)){
+            i++;
         }
+        poccess(argv,i);
+        
         free(cmdline);
     }
     return 0;
@@ -72,6 +64,29 @@ int syn_err(char *msg)
     return -1;
 }
 
+void poccess(char *argv[],int cnt){
+    int flag=isdo(argv,cnt);
+    if(pass==1){
+        cnt--;
+    }
+    if(flag==1){
+        mycd(argv);
+    }else if(strcmp(argv[0],"exit")==0){
+        printf("exit\n有停止的任务\n");
+        exit(0);
+    }else if(flag==2){
+        mydup(argv);
+    }else if(flag==3){
+        mypipe(argv);
+    }else if(flag==4){
+        mydup2(argv);
+    }else if(flag==5){
+        mydup3(argv);
+    }else if(flag==10){
+        execute(argv);
+    }
+}
+
 int execute(char *argv[])
 {
     int pid;
@@ -96,88 +111,6 @@ int execute(char *argv[])
     return child_info;
 }
 
-int ok_to_execute()
-{
-    int rv=1;
-    if(if_state==WANT_THEN){
-        syn_err("then expected");
-        rv=0;
-    }else if(if_state==THEN_BLOCK&&if_result==SUCCESS){
-        rv=1;
-    }else if(if_state==THEN_BLOCK&&if_result==FAIL)
-    {
-        rv=0;
-    }
-    return rv;
-
-}
-
-int is_control_command(char *s)//判断if等语句
-{
-    return(strcmp(s,"if")==0||strcmp(s,"then")==0||strcmp(s,"fi")==0);
-}
-
-int do_control_command(char **args)
-{
-    char *cmd=args[0];
-    int rv=-1;
-
-    if(strcmp(cmd,"if")==0){
-        if(if_state!=NEUTRAL){
-             rv=syn_err("if unexpected");
-        }else{
-            last_stat=process(args+1);
-            if_result=(last_stat==0?SUCCESS:FAIL);
-            if_state=WANT_THEN;
-            rv=0;
-        }
-
-    }else if(strcmp(cmd,"then")==0){
-        if(if_state!=WANT_THEN){
-            rv=syn_err("then unexpected");
-
-        }else{
-            if_state=THEN_BLOCK;
-            rv=0;
-        }
-    }else if(strcmp(cmd,"else")==0){
-        if(if_state!=WANT_THEN){
-            rv=syn_err("else unexpected");
-
-        }else{
-            if_state=THEN_BLOCK;
-            rv=0;
-        }
-    }else if(strcmp(cmd,"fi")==0){
-        if(if_state!=THEN_BLOCK){
-            rv=syn_err("fi unexpected");
-
-        }else{
-            if_state=NEUTRAL;
-            rv=0;
-        }
-    }
-    else{
-        fatal("internal error processing:",cmd,2);
-        return rv;
-    }
-
-}
-
-
-int process(char ** args)//if then的处理函数
-{
-    int rv=0;
-    if(args[0]==NULL)
-    rv=0;
-    else if(is_control_command(args[0])){
-        rv=do_control_command(args);
-    }
-    else if(ok_to_execute()){//没有if
-        rv=execute(args);
-    }
-    return rv;//返回状态值，0代表没有相关语句，1代表有
-}
 
 
 char *next_cmd(char *prompt,FILE *fp)
@@ -210,45 +143,7 @@ char *next_cmd(char *prompt,FILE *fp)
 
 
 
-char **splitline(char*line)//将字符串分解为字符串数组，并返回这个数组，调用malloc分配内存可以接受任意参数个数的命令行
-{
-    char *newstr();
-    char **args;
-    int spots=0;
-    int bufspace=0;
-    int argnum=0;
-    char *cp=line;
-    char *start;
-    int len;
 
-    if(line==NULL)
-        return NULL;
-
-    args=emalloc(BUFSIZ);
-    bufspace=BUFSIZ;
-    spots=BUFSIZ/sizeof(char*);
-
-    while(*cp!='\0'){
-        while(is_delim(*cp))
-            cp++;//空格或者\t跳过
-        if(*cp=="\0")
-            break;//结束
-        if(argnum+1>=spots)
-        {
-            args=erealloc(args,bufspace+BUFSIZ);
-            bufspace+=BUFSIZ;
-            spots+=(BUFSIZ/sizeof(char*));
-        }
-
-        start=cp;
-        len=1;
-        while(*++cp!='\0'&&!(is_delim(*cp)))
-            len++;
-        args[argnum++]=newstr(start,len);//分解字符串
-    }
-    args[argnum]=NULL;
-    return args;//返回指向指针的指针，指针为参数命令
-}
 
 void *emalloc(size_t n)
 {
@@ -283,7 +178,65 @@ void *erealloc(void *p,size_t n)
     return rv;
 }
 
-            
+//管道fd[0]为读端，fd[1]为写端,pipe单向，有名管道双向
+//在父进程中，fork返回新创建子进程的进程ID；在子进程中，fork返回0；    
 
 
 
+int isdo(char *argv[],int cnt){
+    int flag=10,i;
+    if(argv[0]==NULL){
+        return 0;
+    }
+    if(strcmp(argv[0],"cd")==0){
+        flag=1;
+    }
+    for(i=0;i<cnt;i++){
+        if(strcmp(argv[0],">")==0){
+            flag=2;
+        }
+        if(strcmp(argv[0],"|")==0){
+            flag=3;
+        }
+        if(strcmp(argv[0],">>")==0){
+            flag=4;
+        }
+        if(strcmp(argv[0],"<")==0){
+            flag=5;
+        }
+        if(strcmp(argv[0],"<<")==0){
+            flag=6;
+        }
+        if(strcmp(argv[0],"&")==0){
+            pass=1;
+            argv[i]=NULL;
+        }
+    }
+    return flag;
+
+}
+
+char strcwd[MAX]
+void mycd(const char *argv[]){
+    if(argv[1]==NULL){
+        getcwd(strcwd,sizeof(strcwd));
+        chdir("/home");
+    }else if(strcmp(argv[1],"~")==0){
+        getcwd(strcwd,sizeof(strcwd));
+        chdir("/home/zhuheqin");
+
+    }else if(strcmp(argv[1],"-")==0){//返回进入此目录之前所在的目录
+        char strcwd1[MAX];
+        getcwd(strcwd1,sizeof(strcwd1));
+        chdir(strcwd);
+        printf("%s\n",strcwd);
+        strcpy(strcwd,strcwd1);
+    }else{
+        getcwd(strcwd,sizeof(strcwd));
+        chdir(argv[1]);
+    }
+    
+
+}
+
+void mydup()//重定向使输出输出到文件中
