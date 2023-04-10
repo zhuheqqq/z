d5171edcea56831ec465ca3d99d1e64fb15db38c
zhu-to-u <2528487925@qq.com>
2023-04-10 15:44:46 +0800

    简单的连接线程

---
+++ 多线程/simple_thread.c
@@ -0,0 +1,34 @@
+#include<pthread.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+static void*threadFunc(void *arg)
+{
+    char *s=(char *)arg;
+    printf("%s",s);
+
+    return (void*)strlen(s);
+}
+
+int main()
+{
+    pthread_t t1;
+    void *res;
+    int s;
+
+    s=pthread_create(&t1,NULL,threadFunc,"hello world\n");
+    // if(s!=0){
+    //     errExitEN(s,"pthread_create");
+    // }
+
+    printf("Message from main()\n");
+    s=pthread_join(t1,&res);
+    // if(s!=0){
+    //     errExitEN(s,"pthread_join");
+    // }
+
+    printf("Thread returned %ld\n",(long)res);
+
+    exit(EXIT_SUCCESS);
+}
