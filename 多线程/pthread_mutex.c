2cb222aff1aa3ae3519ee83be8fd314c0c09c833
zhu-to-u <2528487925@qq.com>
2023-04-11 21:57:16 +0800

    pthread_mutex

--- 多线程/pthread_mutex.c
+++ 多线程/pthread_mutex.c
@@ -4,7 +4,7 @@
 #include<unistd.h>
 #include<string.h>
 
-pthread_mutex_t mutex;//定义一把互斥锁
+pthread_mutex_t mutex;//定义一把互斥锁   可以认为是一个整数
 
 void *tfn(void *arg)
 {
@@ -12,12 +12,12 @@ void *tfn(void *arg)
 
     while(1)
     {
-        pthread_mutex_lock(&mutex);//加锁
+        pthread_mutex_lock(&mutex);//加锁   想象成锁--
         printf("hello");
         sleep(rand()%3);//模拟长时间操作共享资源,导致cpu易主,产生与时间有关的错误
         printf("world\n");
 
-        pthread_mutex_unlock(&mutex);//解锁
+        pthread_mutex_unlock(&mutex);//解锁  锁++
 
         sleep(rand()%3);
     }
@@ -29,7 +29,7 @@ int main(void){
     pthread_t tid;
     srand(time(NULL));
 
-    int ret=pthread_mutex_init(&mutex,NULL);//初始化互斥锁
+    int ret=pthread_mutex_init(&mutex,NULL);//初始化互斥锁  可以认为锁的值为1
     if(ret!=0){
         fprintf(stderr,"mutex init error:%s\n",strerror(ret));
         exit(1);
