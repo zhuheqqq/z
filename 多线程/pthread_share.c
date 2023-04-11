310b7637fb5de3074f495aadffc3f631868cf59a
zhu-to-u <2528487925@qq.com>
2023-04-11 15:43:32 +0800

    模拟长时间操作共享资源,导致cpu易主,产生与时间有关的错误

---
+++ 多线程/pthread_share.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<unistd.h>
+#include<string.h>
+
+void *tfn(void *arg)
+{
+    srand(time(NULL));
+
+    while(1)
+    {
+        printf("hello");
+        sleep(rand()%3);//模拟长时间操作共享资源,导致cou易主,产生与时间有关的错误
+        printf("world\n");
+        sleep(rand()%3);
+    }
+
+    return NULL;
+}
+
+int main(void){
+    pthread_t tid;
+    srand(time(NULL));
+
+    pthread_create(&tid,NULL,tfn,NULL);
+
+    while(1){
+        printf("HELLO");
+        sleep(rand()%3);
+        printf("WORLD\n");
+        sleep(rand()%3);
+    }
+
+    pthread_join(tid,NULL);
+
+}
