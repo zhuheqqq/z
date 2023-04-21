7294f4e5e235e913511d3566953c188883109520
zhu-to-u <2528487925@qq.com>
2023-04-21 21:01:13 +0800

    删除链表的倒数第n个节点

--- 力扣/19.c
+++ 力扣/19.c
@@ -43,3 +43,30 @@ struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
     
 
 }
+
+//快慢指针解法
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
+    //设置哑结点
+    struct ListNode* dummyHead = malloc(sizeof(struct ListNode));
+    dummyHead->next = head;
+    //设置双指针
+    struct ListNode* fast = dummyHead;
+    struct ListNode* slow = dummyHead;
+    //fast 先走 N+1 步
+    while (n--) {
+        fast = fast->next;
+    }
+    fast = fast->next;
+    //fast 和 slow 同时向后移动直到 fast 指向 NULL
+    while (fast) {
+        fast = fast->next;
+        slow = slow->next;
+    }
+    //删除结点并释放结点空间
+    struct ListNode* temp = slow->next;
+    slow->next = slow->next->next;
+    free(temp);
+    //注意返回的是 dummy->next 而不是 head
+    return dummyHead->next;
+}
+
