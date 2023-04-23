4a7a33f8bfda25a7bacf21bfb82e793b200d40ca
zhu-to-u <2528487925@qq.com>
2023-04-23 17:25:36 +0800

    Single-producer_single-consumer_Queue

--- 多线程/sem_Thread.c
+++ 多线程/sem_Thread.c
@@ -1,17 +1,15 @@
-// Single-producer , single-consumer Queue
-//借助条件变量模拟生产者消费者问题
-#include<stdio.h>
-#include<unistd.h>
-#include<pthread.h>
+//利用信号量实现单生产者消费者模型
 #include<stdio.h>
 #include<stdlib.h>
-#include<errno.h>
+#include<unistd.h>
+#include<pthread.h>
+#include<string.h>
+#include<semaphore.h>
 
-int i=0;//生产者序号
-int j=0;//消费者序号
-#define SIZE 10//最大容纳量
+#define NUM 5
+int i=0;
+int j=0;
 
-// 链表作为共享数据,需被互斥量保护
 struct SPSCQueue {
    int num;
    struct SPSCQueue *next;
@@ -19,10 +17,11 @@ struct SPSCQueue {
 } typedef SPSCQueue;
 
 struct SPSCQueue *head;//头指针
+sem_t blank_number,product_number;//空格子信号量,产品信号量
 
 //静态初始化一个条件变量和一个互斥量
-pthread_cond_t has_product=PTHREAD_COND_INITIALIZER;
-pthread_mutex_t lock=PTHREAD_MUTEX_INITIALIZER;
+// pthread_cond_t has_product=PTHREAD_COND_INITIALIZER;
+// pthread_mutex_t lock=PTHREAD_MUTEX_INITIALIZER;
 
 //生产者
 void *producer(void *arg)
@@ -30,25 +29,23 @@ void *producer(void *arg)
     SPSCQueue *temp;
     for(;;)
     {
+        sem_wait(&blank_number);//生产者将空格数--,为0阻塞等待
         temp=malloc(sizeof(SPSCQueue));
         temp->num=rand()%1000+1;//模拟生产一个产品
         head->cnt++;
         printf("-第%d个Produce %lu------------------------------%d\n",++i,pthread_self(),temp->num);//输出提示信息
         
-        pthread_mutex_lock(&lock);//加锁
+        //pthread_mutex_lock(&lock);//加锁
 
-        if(temp->cnt==SIZE){//队列已满
-            pthread_cond_wait(&has_product,&lock);//生产者阻塞等待,pthread_cond_wait返回时,重写加锁mutex
-        }
+        
         
         SPSCQueue *p;
         p=head->next;
         head->next=temp;
         temp->next=p;//头插法入队列
 
-        pthread_mutex_unlock(&lock);//解锁
+        sem_post(&product_number);//将产品数++
 
-        pthread_cond_signal(&has_product);//将等待在该条件变量上的一个线程唤醒
 
         sleep(rand()%5);
     }
@@ -62,13 +59,7 @@ void *consumer(void *arg)
 
     for(;;)
     {
-        pthread_mutex_lock(&lock);//消费者加锁
-
-        if(head->next==NULL)//多线程阻塞时,可能会出现线程被唤醒但队列为空的情况,所以需要循环判断
-        {
-            pthread_cond_wait(&has_product,&lock);//消费者阻塞等待,pthread_cond_wait返回时,重写加锁mutex
-
-        }
+       sem_wait(&product_number);//消费者将产品数--,为0则阻塞等待
 
         temp=head;
         while(temp->next->next!=NULL)//!找到尾节点上一个节点
@@ -78,8 +69,8 @@ void *consumer(void *arg)
 
         printf("-第%d个Cosumer %lu-----------------------------%d\n",++j,pthread_self(),temp->next->num);//输出提示信息
         head->cnt--;
-        pthread_mutex_unlock(&lock);//解锁
-        pthread_cond_signal(&has_product);//唤醒线程
+
+        sem_post(&blank_number);//消费掉以后,将空格数++
 
         free(temp->next);
         temp->next=NULL;//这一步很重要,否则segmentation fault
@@ -98,7 +89,20 @@ int main(int argc,char *argv[])
     pthread_t pid,cid;
     srand(time(NULL));
 
-    int ret=pthread_create(&pid,NULL,producer,NULL);
+    int ret=sem_init(&blank_number,0,NUM);//初始化空格信号量为5,线程间共享--0
+    if(ret!=0)
+    {
+        fprintf(stderr,"pthread_create error:%s\n",strerror(ret));
+        exit(1);
+    }
+    ret=sem_init(&product_number,0,0);//产品数为0
+    if(ret!=0)
+    {
+        fprintf(stderr,"pthread_create error:%s\n",strerror(ret));
+        exit(1);
+    }
+
+    ret=pthread_create(&pid,NULL,producer,NULL);
     if(ret!=0)
     {
         fprintf(stderr,"pthread_create error:%s\n",strerror(ret));
@@ -126,17 +130,17 @@ int main(int argc,char *argv[])
         exit(1);
     }
 
-    ret=pthread_mutex_destroy(&lock);
+    ret=sem_destroy(&blank_number);
     if(ret!=0)
     {
-        fprintf(stderr,"pthread_mutex_destroy error:%s\n",strerror(ret));
+        fprintf(stderr,"sem_destroy error:%s\n",strerror(ret));
         exit(1);
     }
 
-    ret=pthread_cond_destroy(&has_product);
+    ret=sem_destroy(&product_number);
     if(ret!=0)
     {
-        fprintf(stderr,"pthread_cond_destroy error:%s\n",strerror(ret));
+        fprintf(stderr,"sem_destroy error:%s\n",strerror(ret));
         exit(1);
     }
 
